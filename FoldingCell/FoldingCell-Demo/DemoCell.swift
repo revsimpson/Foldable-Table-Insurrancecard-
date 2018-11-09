ddce5375641fedeaf1537efd0bf0900821e1a6f4
Richard Simpson <rsimpson@dsw.nl>
2018-11-09 09:22:01 +0100

    Basic prototype of verzekerdenpassen

---
+++ FoldingCell/FoldingCell-Demo/DemoCell.swift
@@ -0,0 +1,41 @@
+//
+//  DemoCell.swift
+//  FoldingCell
+//
+
+
+import FoldingCell
+import UIKit
+
+class DemoCell: FoldingCell {
+
+    @IBOutlet var closeNumberLabel: UILabel!
+    @IBOutlet var openNumberLabel: UILabel!
+
+    var number: Int = 0 {
+        didSet {
+            closeNumberLabel.text = String(number)
+            openNumberLabel.text = "Verzekerde informatie" 
+        }
+    }
+
+    override func awakeFromNib() {
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        super.awakeFromNib()
+    }
+
+    override func animationDuration(_ itemIndex: NSInteger, type _: FoldingCell.AnimationType) -> TimeInterval {
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+}
+
+// MARK: - Actions ⚡️
+
+extension DemoCell {
+
+    @IBAction func buttonHandler(_: AnyObject) {
+        print("tap")
+    }
+}
